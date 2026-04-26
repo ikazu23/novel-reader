fb6bd2a660db6a3e633fd493593476351e05ca4e
ikazu23 <162088777+ikazu23@users.noreply.github.com>
2026-04-26 16:01:31 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // 小説リーダー Service Worker
 // 更新時はこのバージョン番号を上げる
-const CACHE_VERSION = 'v13';
+const CACHE_VERSION = 'v14';
 const CACHE_NAME = `novel-reader-${CACHE_VERSION}`;
 
 // 完全オフライン用にキャッシュするリソース
